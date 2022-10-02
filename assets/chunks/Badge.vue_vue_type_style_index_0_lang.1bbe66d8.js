347ce29a97ae5f2631b9795b6986f5b48b2cf6be
wibus-wee <wibus-wee@users.noreply.github.com>
2022-10-02 02:33:22 +0000

    deploy: bf9ae14ecc40dfdb37eaaf83810e035b292461e5

---
+++ assets/chunks/Badge.vue_vue_type_style_index_0_lang.1bbe66d8.js
@@ -0,0 +1,1 @@
+import{g as t,o as a,c as n,t as o,n as r}from"../app.e247d283.js";const _=t({__name:"Badge",props:{text:{type:String,required:!0},color:{type:String,default:"blue"}},setup(e){return(s,c)=>(a(),n("span",{class:r(["badge",e.color])},o(e.text),3))}});export{_};
