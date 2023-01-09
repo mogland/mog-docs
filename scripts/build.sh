b1692d9b0e6c05a017aaa73f66a8930187ce95f6
wibus-wee <1596355173@qq.com>
2023-01-09 10:38:20 +0800

    fix: remove index page

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,17 +3,17 @@ pnpm run build
 mkdir -p dist
 cp -r src/.vitepress/dist/* dist/
 echo 'mog.js.org' > dist/CNAME
-rm -rf dist/index.html # 移除 vitepress 生成的 index.html
+# rm -rf dist/index.html # 移除 vitepress 生成的 index.html
 
 # in index page
-git clone https://github.com/mogland/mog.js.org.git
-cd mog.js.org
-pnpm i
-pnpm run build
-cd ..
-cp -r mog.js.org/out/* dist/
+# git clone https://github.com/mogland/mog.js.org.git
+# cd mog.js.org
+# pnpm i
+# pnpm run build
+# cd ..
+# cp -r mog.js.org/out/* dist/
 
-rm -rf mog.js.org
+# rm -rf mog.js.org
 
 # in blog
 git clone https://github.com/mogland/blog.git mog-blog
